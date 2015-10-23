09666cadf1a03de324b75f00f62fcf4e2852b6a8
rdgifford <robbie.gifford@gmail.com>
2015-10-22 23:01:13 -0700

    Twilio testing.

--- config/application.rb
+++ config/application.rb
@@ -17,7 +17,7 @@ module DDS
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     config.time_zone = 'Pacific Time (US & Canada)'
     config.active_record.default_timezone :local
-
+    config.assets.initialize_on_precompile = false
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
